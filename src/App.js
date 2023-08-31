08a25adc7a8b93b94fc34492c112ac1011fd747a
Precious <Preshomondi@gmail.com>
2023-08-31 08:56:50 +0300

    Create routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+import {Routes, Route, Outlet, Navigate} from 'react-router-dom'
 import Team from "./components/Team";
 import Home from "./components/Home";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Projects from "./components/Projects";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
   return (
     <div className="app">
-      <Home/>
-      <Team/>
+      {loading ? (<Loader/>) : (
+        <Routes>
+          <Route element={<Navigate to="/home"/>} path="/" exact></Route>
+          <Route element={<Projects/>} path="/projects"></Route>
+          <Route element={<About/>} path="/about"></Route>
+          <Route element={<Contacts/>} path='/contacts'></Route>
+          <Route element={<Team/>} path="/team"></Route>
+          <Route element={<Home/>} path="/home"></Route>
+        </Routes>
+      )}
      
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+const navLinks = ["Home", "About", "Projects", "Team", "Contacts"];
 
 function Home() {
-  const header = "/assets/header.jpeg"
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const header = "/assets/header.jpeg";
   const inlineStyles = {
     backgroundImage: `url(${header})`,
     backgroundRepeat: "no-repeat",
-    backgroundPosition: 'center',
+    backgroundPosition: "center",
     backgroundSize: "cover",
-    backgroundAttachment: 'fixed',
-  }
+    backgroundAttachment: "fixed",
+  };
   return (
     <div className="home" style={inlineStyles}>
       <div className="opaque">
-      <div className="navbar">
-        <div className="logo">
-          <h3>Quasar</h3>
+        <div className="navbar">
+          <div className="logo">
+            <h3>Quasar</h3>
+          </div>
+          <div className="nav">
+            <ul>
+              {navLinks.map((navLink) => (
+                <li 
+                onClick={() => navigate(`/${navLink.toLowerCase()}`)}
+                className = {
+                  pathname === `/${navLink.toLowerCase()}` ? "active-li" : ""
+                }
+                >{navLink}</li>
+              ))}
+            </ul>
+          </div>
+          <button className="tel">
+            <FontAwesomeIcon icon={faPhone} className="phone" />
+            <span className="tel-text">Call Us</span>
+          </button>
         </div>
-        <div className="nav">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Team</li>
-            <li>Contacts</li>
-          </ul>
-        </div>
-        <button className="tel">
-          <FontAwesomeIcon icon={faPhone} className="phone"/>
-        <span className="tel-text">
-          Call Us
-        </span>
-        </button>
       </div>
-
-      </div>
-     
     </div>
   );
 }
