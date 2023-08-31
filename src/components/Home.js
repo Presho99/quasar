ea7298c306cf925c86214a603fdecd69ee7a8892
Precious <Preshomondi@gmail.com>
2023-08-31 07:22:26 +0300

    Install font-awesome

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Home.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
   const header = "/assets/header.jpeg"
@@ -13,11 +15,11 @@ function Home() {
   return (
     <div className="home" style={inlineStyles}>
       <div className="opaque">
-
-      </div>
       <div className="navbar">
-        <div className="logo"></div>
-        <div className="nav-itself">
+        <div className="logo">
+          <h3>Quasar</h3>
+        </div>
+        <div className="nav">
           <ul>
             <li>Home</li>
             <li>About</li>
@@ -26,7 +28,13 @@ function Home() {
             <li>Contacts</li>
           </ul>
         </div>
+        <div className="tel">
+          <FontAwesomeIcon icon={faPhone}/>
+        </div>
       </div>
+
+      </div>
+     
     </div>
   );
 }
