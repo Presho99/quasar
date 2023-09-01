b225342cc7a2d0c347ba10719e8da3d7b34ffc9b
Precious <Preshomondi@gmail.com>
2023-09-01 23:52:19 +0300

    Add code

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './About.css'
+import { useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faXmark,
+  faArrowRight,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
+
+const navLinks = ["Home", "About", "Projects", "Team", "Contacts"];
+
 
 function About() {
+    const [isMenu, setIsMenu] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const showMenu = () => {
+    setIsMenu(!isMenu);
+  };
   return (
-    <div>About</div>
+    <div className='about'>
+        <div className='blob'></div>
+        {isMenu && (
+        <div className="small-menu">
+          <div className="small-menu-card">
+            <button className="button-xmark x">
+              <FontAwesomeIcon
+                icon={faXmark}
+                className="xmark"
+                onClick={showMenu}
+              />
+            </button>
+            <ul>
+              {navLinks.map((navLink) => (
+                <li
+                  onClick={() => navigate(`/${navLink.toLowerCase()}`)}
+                  className={
+                    pathname === `/${navLink.toLowerCase()}` ? "active-li" : ""
+                  }
+                >
+                  {navLink}
+                </li>
+              ))}
+            </ul>
+          
+          </div>
+        </div>
+      )}
+       <div className="top-menu">
+        <div className="logo">
+          <h4>Precious Omondi&#169;</h4>
+        </div>
+        <div className="nav">
+          {isSmallScreen ? (
+            <FontAwesomeIcon
+              icon={faBars}
+              onClick={showMenu}
+              style={{ cursor: "pointer" }}
+            />
+          ) : (
+            <ul>
+              {navLinks.map((navLink) => (
+                <li
+                  onClick={() => navigate(`/${navLink.toLowerCase()}`)}
+                  className={
+                    pathname === `/${navLink.toLowerCase()}` ? "active-li" : ""
+                  }
+                >
+                  {navLink}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,66 @@
-import React from 'react'
-import './Contacts.css'
+import "./Contacts.css";
+import React, { useEffect, useState } from "react";
+import { FaFacebook, FaInstagram, FaWhatsapp, FaTwitter } from "react-icons/fa";
+import { useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+const navLinks = ["Home", "About", "Projects", "Team", "Contacts"];
 
 function Contacts() {
+  const [isMenu, setIsMenu] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const header = "/assets/skyline.jpeg";
+  const inlineStyles = {
+    backgroundImage: `url(${header})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "bottom",
+    backgroundSize: "cover",
+  };
+
   return (
-    <div>Contacts</div>
-  )
+    <div className="contacts">
+       <div className="overlay"></div>
+      <div className="contact-hero" style={inlineStyles}>
+        <div className="navbar">
+          <div className="logo">
+            <h3>Quasar</h3>
+          </div>
+          <div className="nav">
+            <ul>
+              {navLinks.map((navLink) => (
+                <li
+                  onClick={() => navigate(`/${navLink.toLowerCase()}`)}
+                  className={
+                    pathname === `/${navLink.toLowerCase()}` ? "active-li" : ""
+                  }
+                >
+                  {navLink}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <h3>Contact Us</h3>
+      </div>
+      <div className="contact-middle">
+        <div className="map"></div>
+        <div className="contact-form">
+          
+        </div>
+      </div>
+     
+      <div className="footer">
+        <div className="footer-text">
+          <h4>Quasar</h4>
+          <p>Energy Limited</p>
+        </div>
+      </div>
+      <div className="name"></div>
+    </div>
+  );
 }
 
-export default Contacts
+export default Contacts;
