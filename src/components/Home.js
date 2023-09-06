6b35de0fb04e7411d493f48704d8f6658f3bcf55
Precious <Preshomondi@gmail.com>
2023-09-06 19:43:47 +0300

    Add responsive styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ import "./Home.css";
 import { FaFacebook, FaInstagram, FaWhatsapp, FaTwitter } from "react-icons/fa";
 import { useNavigate, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
 
 const navLinks = ["Home", "About", "Safety", "Contacts"];
 
 function Home() {
-  const [isMenu, setIsMenu] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const navigate = useNavigate();
   const { pathname } = useLocation();
@@ -29,9 +29,41 @@ function Home() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div className="home" style={inlineStyles}>
+        {isMenuOpen && (
+          <div className="small-menu">
+            <div className="small-menu-card">
+              <button className="button-xmark x">
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  className="xmark"
+                  onClick={toggleMenu}
+                />
+              </button>
+              <ul>
+                {navLinks.map((navLink) => (
+                  <li
+                    onClick={() => navigate(`/${navLink.toLowerCase()}`)}
+                    className={
+                      pathname === `/${navLink.toLowerCase()}`
+                        ? "active-li"
+                        : ""
+                    }
+                  >
+                    {navLink}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
       <div className="frosted">
+      
         <div className="socials">
           <a href="#">
             <FaFacebook />
@@ -49,38 +81,53 @@ function Home() {
       </div>
       <div className="text">
         <h1>Innovative Gas Solutions Ahead</h1>
-        <p>Advanced gas technologies making clean energy accessible to everyone.</p>
-        <button onClick={() => navigate('/about')}><p>Read More</p></button>
-    
+        <p>
+          Advanced gas technologies making clean energy accessible to everyone.
+        </p>
+        <button onClick={() => navigate("/about")}>
+          <p>Read More</p>
+        </button>
       </div>
       <div className="opaque">
         <div className="navbar">
           <div className="logo">
-            <h3>Quasar</h3>
+           
+<h3>Quasar</h3>
+            
+            
           </div>
           <div className="nav">
-            <ul>
-              {navLinks.map((navLink) => (
-                <li
-                  onClick={() => navigate(`/${navLink.toLowerCase()}`)}
-                  className={
-                    pathname === `/${navLink.toLowerCase()}` ? "active-li" : ""
-                  }
-                >
-                  {navLink}
-                </li>
-              ))}
-            </ul>
+            {isSmallScreen ? (
+              <FontAwesomeIcon
+                icon={faBars}
+                onClick={toggleMenu}
+                style={{ cursor: "pointer" }}
+              />
+            ) : (
+              <ul>
+                {navLinks.map((navLink) => (
+                  <li
+                    onClick={() => navigate(`/${navLink.toLowerCase()}`)}
+                    className={
+                      pathname === `/${navLink.toLowerCase()}`
+                        ? "active-li"
+                        : ""
+                    }
+                  >
+                    {navLink}
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
           <button className="tel" onClick={() => navigate(`/contacts`)}>
             <FontAwesomeIcon icon={faPhone} className="phone" />
             <span className="tel-text">Call Us</span>
           </button>
         </div>
-        
       </div>
-   
-     <div className="caret-right"  onClick={() => navigate(`/about`)}></div>
+
+      <div className="caret-right" onClick={() => navigate(`/about`)}></div>
     </div>
   );
 }
