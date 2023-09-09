850b8990e0e0369f1531481fb01e5c9a5d445a7a
Precious <Preshomondi@gmail.com>
2023-09-09 19:23:24 +0300

    Adjust loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Loader.css'
+import React from "react";
+import "./Loader.css";
 
 function Loader() {
   return (
-    <div className='loader-container'>
-      <img src="/assets/logo.png" className='loader-logo' alt="logo"/>
-      <div className='loader-text'>
-        <h3>Quasar</h3>
-        <p>Energy Limited</p>
+    <div className="loader">
+      <div className="loader-container">
+        <img src="/assets/logo.png" className="loader-logo" alt="logo" />
+        <div className="loader-text">
+          <h3>Quasar</h3>
+          <p>Energy Limited</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Loader
+export default Loader;
